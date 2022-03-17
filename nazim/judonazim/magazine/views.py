23aeba87e98eb870584f9d49f8f3de02d9fa073a
ronnythenazi <ronnythenazi@gmail.com>
2022-03-17 23:02:53 +0200

    fixing bug in magazine home page, server 500 error, because of inconssitance between context key name, author_post in magazin.html vs authors_posts in view

--- nazim/judonazim/magazine/views.py
+++ nazim/judonazim/magazine/views.py
@@ -151,9 +151,9 @@ class MagazineHome(ListView):
                 authors_id.append(author[0])
         #print(authors_id)
 
-        context['authors_posts'] = []
+        context['author_posts'] = []
         for author_id in authors_id:
-            context['authors_posts'].append(BlogPost.objects.filter(publishstatus = 'public').order_by('-datepublished').filter(author__id = author_id)[9:][:min_num_of_posts])
+            context['author_posts'].append(BlogPost.objects.filter(publishstatus = 'public').order_by('-datepublished').filter(author__id = author_id)[9:][:min_num_of_posts])
         #content['author_pubs'] = BlogPost.objects.filter(publishstatus = 'public').order_by('-datepublished')[9:].order_by('author').annotate(authr_posts_num=Count('author', distinct=True)).filter(author__level__gte = 6)
 
         #print(context['authors_posts'][0][0]['pk'])
